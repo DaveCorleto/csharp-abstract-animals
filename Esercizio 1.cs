00b6b61f0b0d937bd5fc8dc9cfb95340a033c88a
DaveCorleto <davide.corleto@fastwebnet.it>
2024-04-24 15:28:43 +0200

    Esercizio 1 terminato

---
+++ Esercizio 1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace csharp_abstract_animals
+{
+    //    ** Esercizio 1 : classi astratte**
+    //Dobbiamo realizzare un programma che rappresenti il regno animale.
+    //Creiamo quindi una classe astratta Animale e delle classi che la estendono : Cane, Passerotto, Aquila, Delfino
+    //Vogliamo che gli animali abbiano i seguenti metodi
+    //- void Dormi() (mostra a video “Zzz”)
+    //- void Verso() (mostra a video il verso fatto dall'animale specifico)
+    //- void Mangia() (mostra a video quello che mangia : erba, carne, ...?)
+    //Quali di questi metodi devono essere implementati nella classe astratta e quali devono essere lasciati astratti e implementati dalle classi che la estendono?
+    //Una volta create le classi, nel programma istanziare alcuni animali e utilizzare i metodi implementati.
+    public abstract class Animale {  
+
+
+    public string razza { get; set; }
+    public string nome { get; set; }
+    public int età { get; set; }
+
+    public Animale (string razza, string nome, int età)
+    {
+        this.razza = razza;
+        this.nome = nome;
+        this.età = età;
+    }
+
+    public abstract void Dormi();
+        public abstract void Verso();
+        public abstract void Mangia();
+
+       
+    }
+    public class Cane : Animale {
+
+        public string verso { get; } = "Bau!";
+
+        public Cane(string razza, string nome, int età) :base(razza,nome,età){ 
+
+        }
+        public override  void Dormi()
+        {
+            Console.WriteLine("Sono un cane e sto dormendo...ZzZzZzZzZ");
+        }
+        public override  void Verso()
+        {
+            Console.WriteLine(verso);
+        }
+        public override void Mangia()
+        {
+            Console.WriteLine("Sono un cane e sto mangiando una bistecca!");
+        }
+    }
+
+    public class Passerotto : Animale {
+
+        public Passerotto (string razza, string nome, int età) : base(razza, nome, età)
+        {
+
+        }
+
+        public string verso = "Cip!";
+        public override void Dormi()
+        {
+            Console.WriteLine("Sono un passerotto e sto dormendo...ZzZzZzZzZ");
+        }
+        public override void Verso()
+       {
+            Console.WriteLine(verso);
+       }
+       public override void Mangia()
+        {
+            Console.WriteLine("Sono un passerotto e sto maniando dei semini!");
+        }
+
+    }
+
+    public class Aquila : Animale {    
+
+           public Aquila(string razza, string nome, int età) : base(razza, nome, età)
+        {
+
+        }
+    
+        public string verso = "CRZVDSILJSPIIIIIIIII!";
+        public override void Dormi()
+        {
+            Console.WriteLine("Sono un aquila e sto dormendo...ZzZzZzZzZ");
+        }
+        public override void Verso()
+        {
+            Console.WriteLine(verso);
+        }
+        public override void Mangia()
+        {
+            Console.WriteLine("Sono un aquila e sto mangiando un castoro!");
+        }
+    }
+
+    public class Delfino : Animale
+    {
+        public Delfino (string razza, string nome, int età) : base(razza, nome, età)
+        {
+
+        }
+
+        public string verso = "Hik hik!";
+        public override void Dormi()
+        {
+            Console.WriteLine("Sono un Delfino e sto dormendo...ZzZzZzZzZ");
+        }
+        public override void Verso()
+        {
+            Console.WriteLine(verso);
+        }
+        public override void Mangia()
+        {
+            Console.WriteLine("Sono un Delfino e sto mangiando del pesce!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Cane Holly = new Cane("Labrador", "Holly", 6);
+            Console.WriteLine($"Ecco un nuovo animale: si chiama {Holly.nome}, è un {Holly.razza}, ha {Holly.età} anni. Ora farà cose...");
+            Holly.Mangia();
+            Holly.Dormi();
+            Holly.Verso();
+            Console.WriteLine("\n\n\n\n\n\n");
+
+            Passerotto CippinoCIppotto = new Passerotto("Cinciallegra", "CippinoCippotto", 1);
+            Console.WriteLine($"Ecco un nuovo animale: si chiama {CippinoCIppotto.nome}, è un {CippinoCIppotto.razza}, ha {CippinoCIppotto.età} anni. Ora farà cose...");
+            CippinoCIppotto.Mangia();
+            CippinoCIppotto.Dormi();
+            CippinoCIppotto.Verso();
+            Console.WriteLine("\n\n\n\n\n\n");
+
+            Aquila Dorothy = new Aquila ("Aquila dalla testa bianca", "Dorothy", 10);
+            Console.WriteLine($"Ecco un nuovo animale: si chiama {Dorothy.nome}, è un {Dorothy.razza}, ha {Dorothy.età} anni. Ora farà cose...");
+            Dorothy.Mangia();
+            Dorothy.Dormi();
+            Dorothy.Verso();
+            Console.WriteLine("\n\n\n\n\n\n");
+
+            Delfino Asdrubale = new Delfino("Tursiope Troncato", "Asdrubale", 4);
+
+            Console.WriteLine($"Ecco un nuovo animale: si chiama {Asdrubale.nome}, è un {Asdrubale.razza}, ha {Asdrubale.età} anni. Ora farà cose...");
+            Asdrubale.Mangia();
+            Asdrubale.Dormi();
+            Asdrubale.Verso();
+            Console.WriteLine("\n\n\n\n\n\n");
+
+
+
         }
     }
 }
